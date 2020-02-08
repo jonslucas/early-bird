a4f1b3e16bec15747f4ebe07706a91c44a08e1ae
Jon Lucas <jon.s.lucas@gmail.com>
2020-02-08 14:09:01 -0500

    feat: Added basic Header component.

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Avatar from './Avatar';
+
+const Header = () => (
+  <div>
+    <div>Tracker</div>
+    <div><Avatar /></div>
+  </div>
+);
+
+export default Header;
