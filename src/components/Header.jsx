00f96f6c068ba93cf2937ffcf5ba33ef74999b69
Jon Lucas <jon.s.lucas@gmail.com>
2020-02-15 12:11:53 -0500

    feat: Wired in material ui to Header.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,38 @@
 import React from 'react';
-import './Header.css';
+import { makeStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
 import Avatar from './Avatar';
 
-const Header = () => (
-  <div className="container">
-    <div className="app-name"><h2>Tracker</h2></div>
-    <div className="avatar"><Avatar /></div>
-  </div>
-);
+const useStyles = makeStyles({
+  root: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    borderBottom: 'solid #80808045 2px',
+    width: '100%',
+    padding: '5px 10px 5px 10px',
+    maxHeight: '50px',
+  },
+  appName: {
+    display: 'flex',
+    justifyContent: 'flex-start',
+  },
+  avatar: {
+    display: 'flex',
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+});
+
+const Header = () => {
+  const style = useStyles();
+  return (
+    <div className={style.root}>
+      <div className={style.appName}>
+        <Typography variant="h4">Tracker</Typography>
+      </div>
+      <div className={style.avatar}><Avatar /></div>
+    </div>
+  );
+};
 
 export default Header;
