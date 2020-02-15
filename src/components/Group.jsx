d5e3d1abcf22aa6bef41cbf128332f63a2c4311d
Jon Lucas <jon.s.lucas@gmail.com>
2020-02-15 13:40:23 -0500

    feat: Made CardList droppable w/preview.

--- src/components/Group.jsx
+++ src/components/Group.jsx
@@ -7,7 +7,9 @@ import CardActions from '@material-ui/core/CardActions';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
+import { useDrop } from 'react-dnd';
 import CardWrap from './Card';
+import Types from '../state/dnd/types';
 
 const useStyles = makeStyles({
   root: {
@@ -24,24 +26,43 @@ const useStyles = makeStyles({
   },
   content: {
     display: 'flex',
+    flexDirection: 'column',
     backgroundColor: '#d3d3d33b',
   },
   footer: {
     display: 'flex',
     justifyContent: 'center',
   },
+  preview: {
+    height: '5em',
+    width: '100%',
+    opacity: '0.5',
+  },
 });
+
+function moveCard(groupId) {
+  console.log({ groupId });
+}
 
 const Group = ({ name, items }) => {
   const style = useStyles();
+  const [{ isOver }, dropRef] = useDrop({
+    accept: Types.CARD,
+    drop: () => moveCard(name),
+    collect: monitor => ({
+      isOver: !!monitor.isOver(),
+    }),
+  });
+  console.log({ isOver });
   return (
     <Container className={style.root} disableGutters>
-      <Card className="" variant="outlined">
+      <Card className="" variant="outlined" ref={dropRef}>
         <Typography variant="h5" className={style.heading} align="center">
           {name}
         </Typography>
         <CardContent className={style.content}>
           {items.map((i) => (<CardWrap item={i} key={i} />))}
+          {isOver && <Card className={style.preview}></Card>}
         </CardContent>
         <CardActions className={style.footer}>
           <Button size="small">Add Card</Button>
